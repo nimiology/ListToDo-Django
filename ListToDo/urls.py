b6d4ec239ff51a9b28fde6428c9466565b04e630
Nima <nimiologyy@gmail.com>
2021-10-28 13:35:21 +0330

    initial commit

--- ListToDo/urls.py
+++ ListToDo/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
+
+from ListToDo import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    # add media static files
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
