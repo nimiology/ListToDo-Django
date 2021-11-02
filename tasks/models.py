a830b8ab7b3ac52345aa563b578ce1a3b34ff5ed
Matin Rostami <tizmrmr@gmail.com>
2021-11-02 19:05:53 +0330

    formatted and added pipenv

--- tasks/models.py
+++ tasks/models.py
@@ -16,7 +16,8 @@ class Color(models.Model):
 class Label(models.Model):
     related_name = 'labels'
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name=related_name)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name=related_name)
     title = models.CharField(max_length=256)
 
     def __str__(self):
@@ -31,13 +32,18 @@ class Project(models.Model):
         ('B', 'Board'),
     ]
     title = models.CharField(max_length=512)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects_owner')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='projects_owner')
     user = models.ManyToManyField(User, blank=True, related_name=related_name)
-    project = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='subprojects')
+    project = models.ForeignKey(
+        'self', on_delete=models.CASCADE, blank=True, null=True, related_name='subprojects')
     inviteSlug = models.SlugField(blank=True)
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True, blank=True, related_name=related_name)
-    label = models.ManyToManyField(Label, blank=True, related_name=related_name)
-    background = models.ImageField(upload_to=upload_file, blank=True, null=True)
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL,
+                              null=True, blank=True, related_name=related_name)
+    label = models.ManyToManyField(
+        Label, blank=True, related_name=related_name)
+    background = models.ImageField(
+        upload_to=upload_file, blank=True, null=True)
     view = models.CharField(max_length=1, default='L', choices=VIEWS_CHOICES)
     archive = models.BooleanField(default=False)
     position = models.PositiveIntegerField(default=0)
@@ -53,7 +59,8 @@ class Project(models.Model):
 
 class Section(models.Model):
     title = models.CharField(max_length=512)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, related_name='section')
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, blank=True, null=True, related_name='section')
     position = models.PositiveIntegerField(default=0)
 
     def __str__(self):
@@ -70,16 +77,24 @@ class Task(models.Model):
         ('5', 'Priority 5'),
     ]
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task_creator')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
-    assignee = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name=related_name)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
-    task = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='subcategories')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='task_creator')
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
+    assignee = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, related_name=related_name)
+    section = models.ForeignKey(
+        Section, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
+    task = models.ForeignKey('self', on_delete=models.CASCADE,
+                             blank=True, null=True, related_name='subcategories')
     title = models.CharField(max_length=512)
     description = models.TextField(null=True, blank=True)
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True, blank=True, related_name=related_name)
-    label = models.ForeignKey(Label, on_delete=models.SET_NULL, null=True, blank=True, related_name=related_name)
-    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, blank=True, null=True)
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL,
+                              null=True, blank=True, related_name=related_name)
+    label = models.ForeignKey(Label, on_delete=models.SET_NULL,
+                              null=True, blank=True, related_name=related_name)
+    priority = models.CharField(
+        max_length=1, choices=PRIORITY_CHOICES, blank=True, null=True)
     position = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     schedule = models.DateTimeField(blank=True, null=True)
@@ -92,9 +107,12 @@ class Task(models.Model):
 class Comment(models.Model):
     related_name = 'comments'
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name=related_name)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name=related_name)
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE,
+                             blank=True, null=True, related_name=related_name)
     description = models.TextField(null=True, blank=True)
     file = models.FileField(upload_to=upload_file, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -106,8 +124,10 @@ class Comment(models.Model):
 class Activity(models.Model):
     related_name = 'activity'
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name=related_name)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, blank=True, null=True, related_name=related_name)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name=related_name)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE,
+                             blank=True, null=True, related_name=related_name)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
