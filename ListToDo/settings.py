0f0cfa87481d90dc7edcd7a2340c7c36087d3d41
Nima <nimiologyy@gmail.com>
2022-01-30 16:01:01 +0330

    Update settings.py

--- ListToDo/settings.py
+++ ListToDo/settings.py
@@ -95,13 +95,12 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
     'ListToDo.utils.TimezoneMiddleware'
 ]
 
