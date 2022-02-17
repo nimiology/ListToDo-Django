e84b01f6755924e54e91b38542b819c55f654724
Nima <nimiologyy@gmail.com>
2022-02-17 19:31:33 +0330

    Users Serializer

--- users/serializer.py
+++ users/serializer.py
@@ -4,15 +4,6 @@ from rest_framework import serializers
 from users.models import Team, MyUser
 
 
-class TeamSerializer(serializers.ModelSerializer):
-    owner = UserSerializer(required=False, read_only=True)
-    users = UserSerializer(read_only=True, required=False, many=True)
-
-    class Meta:
-        model = Team
-        fields = '__all__'
-
-
 class MyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
@@ -20,3 +11,12 @@ class MyUserSerializer(serializers.ModelSerializer):
                   'header_img', 'timezone', 'setting', 'date_joined']
         extra_kwargs = {'date_joined': {'read_only': True},
                         'username': {'required': False}}
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    owner = MyUserSerializer(required=False, read_only=True)
+    users = MyUserSerializer(read_only=True, required=False, many=True)
+
+    class Meta:
+        model = Team
+        fields = '__all__'
